c62456b07c55ae52d82ad160090afbcc8bd74469
Dongocthach <sheepold2013@gmail.com>
2023-12-05 21:51:14 +0700

    init again

---
+++ app/src/main/java/com/example/duan1bookapp/Constants.java
@@ -0,0 +1,7 @@
+package com.example.duan1bookapp;
+
+public class Constants {
+    public static final String URL_API = "http://192.168.1.10:8080";
+    public static final String URL_IMAGE = "https://firebasestorage.googleapis.com/v0/b/book-app-8a4c9.appspot.com/o/ProfileImages%";
+
+}
